99bd261747bb13566f93965d2f183ad77083c7a0
NecroCorey <75544274+NecroCorey@users.noreply.github.com>
2023-05-09 14:25:21 -0500

    Delete Undernet Trader directory

--- Undernet Trader/init.lua
+++
@@ -1,2 +0,0 @@
-local dat = chaudloader.ExeDat("exe1.dat")  -- MMBN1
-dat:write_file("exe1/labels.bin", chaudloader.modfiles.read_file("labels.bin"))  -- then put your labels.bin replacement in the mod directory
